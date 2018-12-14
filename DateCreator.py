763d6e74c9c9a10145b87f9a8dc7ad1e4acf3b12
babadoctor <nobletabibian@gmail.com>
2018-12-13 19:34:07 -0800

    first commit

---
+++ DateCreator.py
@@ -0,0 +1,30 @@
+import datetime,time,calendar
+dates = []
+epochtime = 1542744000
+numlines = 0
+import os
+
+
+
+print os.path.split("/Users/babadoctor/Desktop/datechecker/dates.txt")
+print calendar.timegm(time.gmtime())#gets current epoch time
+
+
+
+
+with open("/Users/babadoctor/Desktop/datechecker/parsed.sh") as ff:
+    for i, l in enumerate(ff):
+        pass
+    numlines = i + 1
+f = open("/Users/babadoctor/Desktop/datechecker/dates.txt","w+")
+
+
+
+for i in range(numlines):#enumerate through list
+    dates.append(datetime.datetime.fromtimestamp(epochtime).isoformat())#gets iso date from epochtime 
+    epochtime+=86400 #adds a day to epoch time
+    f.write(dates[i]) #write the new date to the array
+    f.write("\n")#newline
+f.close()
+
+
---
+++ LinkParser.py
@@ -0,0 +1,5 @@
+import datetime,time,calendar
+from datetime import datetime, time
+
+
+print datetime.combine(datetime.today(), time.min)
